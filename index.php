6e44942af458cf468c2289e2421ce1558577719a
Anton Ploskirev <anton-plos@yandex.ru>
2019-03-05 20:10:42 +0300

    init repo

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
+</html>
